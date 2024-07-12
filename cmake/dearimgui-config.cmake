65274263372dd204066fc91ad69c5d5bd49c9764
hesuxin <shuxin.he@socialcredits.cn>
2024-07-12 11:46:33 +0800

    FIX: dearimgui-config.cmake wrong variable HIT, should be HINTS

--- cmake/dearimgui-config.cmake
+++ cmake/dearimgui-config.cmake
@@ -1,5 +1,5 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(freetype HIT "$ENV{CMAKE_INSTALL_PREFIX}")
+find_dependency(freetype HINTS "$ENV{CMAKE_INSTALL_PREFIX}")
 
 include("${CMAKE_CURRENT_LIST_DIR}/dearimguiTargets.cmake")
