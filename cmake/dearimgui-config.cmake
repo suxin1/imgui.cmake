bb70406eae638b4bab0f272858a1af960d0297b1
hesuxin <shuxin.he@socialcredits.cn>
2024-07-14 21:56:19 +0800

    update

--- cmake/dearimgui-config.cmake
+++ cmake/dearimgui-config.cmake
@@ -1,5 +1,3 @@
 include(CMakeFindDependencyMacro)
-find_dependency(freetype HINTS "$ENV{CMAKE_INSTALL_PREFIX}")
-find_dependency(freetype HINTS "$ENV{CMAKE_INSTALL_PREFIX}")
 
 include("${CMAKE_CURRENT_LIST_DIR}/dearimguiTargets.cmake")
