d2f5934b531f0f7df7a0cf04e8229e2b6171e791
hesuxin <shuxin.he@socialcredits.cn>
2024-07-20 11:19:37 +0800

    update

--- cmake/dearimgui-config.cmake
+++ cmake/dearimgui-config.cmake
@@ -1,5 +1,7 @@
 include(CMakeFindDependencyMacro)
 
-#find_dependency(freetype HINTS "$ENV{CMAKE_INSTALL_PREFIX}")
+MESSAGE(STATUS ">>>>>>>>" "$ENV{CMAKE_INSTALL_PREFIX}")
+find_dependency(freetype HINTS "$ENV{CMAKE_INSTALL_PREFIX}")
+MESSAGE("Freetype found: " ${freetype_FOUND})
 
 include("${CMAKE_CURRENT_LIST_DIR}/dearimguiTargets.cmake")
